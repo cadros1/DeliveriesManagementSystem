ddca17644af66189805710e7ebd3c050faab2c06
cadros1 <cadros1@outlook.com>
2023-07-12 11:22:07 +0800

    创建了一个Database类,包含一个静态方法readUsersName(),可以读取数据库中的所有用户的名字。以后要增加操作数据库的方法时也应当添加到这个类中。

---
+++ src/main/java/com/gaohongsen/App.java
@@ -0,0 +1,18 @@
+package com.gaohongsen;
+
+import java.sql.*;
+
+/**
+ * 
+ *
+ */
+public class App 
+{
+    public static void main ( String[] args ){
+        try{
+            Database.readUsersName();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/gaohongsen/Database.java
@@ -0,0 +1,22 @@
+package com.gaohongsen;
+
+import java.sql.*;
+
+public class Database {
+    public static void readUsersName ()throws SQLException{
+        String JDBC_URL = "jdbc:mysql://localhost:3306/DataMiningProject?useSSL=false&characterEncoding=utf8";
+        String JDBC_USER = "root";
+        String JDBC_PASSWORD = "800618";
+
+        try(Connection conn=DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASSWORD)){
+            try(PreparedStatement ps=conn.prepareStatement("SELECT name FROM users")){
+                try(ResultSet rs=ps.executeQuery()){
+                    while(rs.next()){
+                        String name=rs.getString("name");
+                        System.out.println(name);
+                    }
+                }
+            }
+        }
+    }
+}
