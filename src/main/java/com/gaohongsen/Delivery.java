9c8a873a978baa2bac6bfa80217291233102c3d6
cadros1 <cadros1@outlook.com>
2023-07-16 10:04:40 +0800

    新增了Delivery类

---
+++ src/main/java/com/gaohongsen/Delivery.java
@@ -0,0 +1,34 @@
+package com.gaohongsen;
+
+public class Delivery implements java.io.Serializable{
+    private int id;
+    private String sendPlace;
+    private String receivePlace;
+    private String sender;
+    private String receiver;
+    private int situation;
+
+    //新增时使用此构造方法
+    public Delivery(String sendPlace, String receivePlace, String sender, String receiver, int situation) {
+        this.sendPlace = sendPlace;
+        this.receivePlace = receivePlace;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.situation = situation;
+    }
+
+    //修改时使用此构造方法
+    public Delivery(int id,String sendPlace, String receivePlace, String sender, String receiver, int situation) {
+        this.id = id;
+        this.sendPlace = sendPlace;
+        this.receivePlace = receivePlace;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.situation = situation;
+    }
+
+    //删除或查询时使用此构造方法
+    public Delivery(int id) {
+        this.id = id;
+    }
+}
